523e9810dc1477822fb3bc201f08a108d04ae83b
dengyancan <1171204763@qq.com>
2019-08-16 13:41:44 +0800

    Configure execution interface

--- interfaceTest/core_package/Configure_even_code.py
+++ interfaceTest/core_package/Configure_even_code.py
@@ -71,6 +71,7 @@ class Interface(unittest.TestCase):
         if self.url and self.data is not None:
 
             try:
+
                 try:
                     logger.info("-" * 49)
                     logger.info("请求方式:%s" % self.method)
@@ -81,19 +82,28 @@ class Interface(unittest.TestCase):
                     logger.info("User_Token: %s" % self.user_token)
                     results = configHttp.runmain.run_main(self.method, self.url, self.data, headers)
                     self.results = results
+
                 except requests.exceptions.ConnectionError as a:
+
                     self.verificationErrors.append(a)
                     return logger.error("login url error : %s" % self.results)
+
                 except TimeoutError as b:
+
                     self.verificationErrors.append(b)
                     return logger.error("login timeout error")
+
                 self.assertEqual(self.results.status_code, requests.codes.OK)
                 logger.info("login is successful")
+
                 r = inheritance.ret.enter(self.results.json(), self.case_name)
                 if r is None:
                     return self.verificationErrors.append(r)
+
                 return
+
             except AssertionError as e:
+
                 self.verificationErrors.append(e)
                 return logger.error("login is %s" % self.results.status_code)
         else:
@@ -104,15 +114,16 @@ class Interface(unittest.TestCase):
         try:
 
             name = xls_name
+            header = headers
             sheet = sheet_name
             dates = readExcel.reds.get_xls(name, sheet)
-            header = headers
 
             if dates is None:
                 self.verificationErrors.append(dates)
                 return logger.error("sheet Interface is %s" % dates)
 
             for method, url, data, case_name in dates:
+
                 try:
                     logger.info("-" * 34)
                     logger.info("请求方式:%s" % method)
@@ -120,22 +131,30 @@ class Interface(unittest.TestCase):
                     logger.info("请求参数:%s" % data)
                     results = configHttp.runmain.run_main(method, url, data, header)
                     self.results = results
+
                 except requests.exceptions.ConnectionError as a:
+
                     self.verificationErrors.append(a)
                     return logger.error("url error : %s" % self.results)
+
                 except TimeoutError as b:
+
                     self.verificationErrors.append(b)
                     return logger.error("timeout error")
+
                 self.assertEqual(self.results.status_code, requests.codes.OK)
                 logger.info("assert url is successful")
+
                 r = inheritance.ret.enter(self.results.json(), case_name)
                 if r is None:
                     return self.verificationErrors.append(r)
 
         except AssertionError as e:
+
             self.verificationErrors.append(e)
             return logger.error("%s assert is error" % self.results.status_code)
 
 
 if __name__ == '__main__':
+
     report.report(Interface, ['login', 'Configure_even_code'])
