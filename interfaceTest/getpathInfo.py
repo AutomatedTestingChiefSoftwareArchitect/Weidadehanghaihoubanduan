29ec0d3857284f8ec8be03152a0d52a6c74fd3c0
dengyancan <1171204763@qq.com>
2019-08-28 16:17:45 +0800

    fix jenkins run path error2

--- interfaceTest/getpathInfo.py
+++ interfaceTest/getpathInfo.py
@@ -3,9 +3,5 @@ import os
 
 def get_Path():
 
-    path = os.path.split(os.path.realpath(__file__))[0]
+    path = '%s' % os.path.split(os.path.realpath(__file__))[0]
     return path
-
-
-if __name__ == '__main__':
-    print('测试路径是否OK,路径为：', get_Path())
