cdc2a90913ff82d72bbf430f789f7ab7c874c9b0
dengyancan <1171204763@qq.com>
2019-08-28 17:18:03 +0800

    fix jenkins run path error9

--- interfaceTest/getpathInfo.py
+++ interfaceTest/getpathInfo.py
@@ -3,13 +3,6 @@ import os
 
 def get_Path():
 
-    # path = '%s' % os.path.split(os.path.realpath(__file__))[0]
-    """
-    // TODO # 指定输出路径 :
-                    1.切勿随意乱push
-                    2.切勿乱执行robot dingding
-    """
-
-    path = "C:/Program Files (x86)/Jenkins/workspace/AutomatedTest/interfaceTest"
+    path = '%s' % os.path.split(os.path.realpath(__file__))[0]
 
     return path
