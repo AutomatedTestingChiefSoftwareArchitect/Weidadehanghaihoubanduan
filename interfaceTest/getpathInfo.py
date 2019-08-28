318aec37540c60a474a7873043d0dfa020ddb313
dengyancan <1171204763@qq.com>
2019-08-28 16:34:18 +0800

    fix jenkins run path error6

--- interfaceTest/getpathInfo.py
+++ interfaceTest/getpathInfo.py
@@ -3,5 +3,13 @@ import os
 
 def get_Path():
 
-    path = '%s' % os.path.split(os.path.realpath(__file__))[0]
+    # path1 = '%s' % os.path.split(os.path.realpath(__file__))[0]
+    """
+    // TODO # 指定输出路径 :
+                    1.切勿随意乱push
+                    2.切勿乱执行robot dingding
+    """
+
+    path = "C:/Program Files (x86)/Jenkins/workspace/AutomatedTest/interfaceTest"
+
     return path
