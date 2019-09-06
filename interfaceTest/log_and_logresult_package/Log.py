e81b78f20adb7e0bb23a114f37c3706a294cc47a
dengyancan <1171204763@qq.com>
2019-09-06 14:36:19 +0800

    fix jenkins checkout code

--- interfaceTest/log_and_logresult_package/Log.py
+++ interfaceTest/log_and_logresult_package/Log.py
@@ -19,7 +19,7 @@ class Logger(object):
         # 定义文件名
         self.log_file_name = 'logs'
         # 保留备份文件个数
-        self.backup_count = 5
+        self.backup_count = 15
         # 日志输出级别
         self.console_output_level = 'WARNING'
         self.file_output_level = 'DEBUG'
@@ -33,20 +33,22 @@ class Logger(object):
             # 日志信息会输出到指定的stream中，如果stream为空则默认输出到sys.stderr
             # logging.StreamHandler(stream=None)
             console_handler = logging.StreamHandler()
+
+            # 每天重新创建一个日志文件，最多保留backup_count份
+            file_handler = TimedRotatingFileHandler(filename=os.path.join(log_path, self.log_file_name), when='midnight',
+                                                    interval=1, backupCount=self.backup_count, delay=True,
+                                                    encoding='utf-8')
+            # 同下 ！！！ file_handler is not console_handler
+            file_handler.setFormatter(self.formatter)
+            file_handler.setLevel(self.file_output_level)
+            self.logger.addHandler(file_handler)
             # 定义最终log信息的顺序, 结构和内容
             console_handler.setFormatter(self.formatter)
             # 定义日志输出级别
             console_handler.setLevel(self.console_output_level)
             # 将相应的console_handler 添加至logger对象中
             self.logger.addHandler(console_handler)
-            # 每天重新创建一个日志文件，最多保留backup_count份
-            file_handler = TimedRotatingFileHandler(filename=os.path.join(log_path, self.log_file_name), when='D',
-                                                    interval=1, backupCount=self.backup_count, delay=True,
-                                                    encoding='utf-8')
-            # 同上 ！！！ file_handler is not console_handler
-            file_handler.setFormatter(self.formatter)
-            file_handler.setLevel(self.file_output_level)
-            self.logger.addHandler(file_handler)
+
         return self.logger
 
 logger = Logger().get_logger()
